86786c0c09f0c8baab7b06a805fa3785898037b2
Christian Baumann <k.c.baumann@t-online.de>
2025-06-02 00:27:19 +0200

    AH-02

---
+++ src/baumann/musik/AH02.java
@@ -0,0 +1,50 @@
+package baumann.musik;
+
+import java.io.IOException;
+import java.util.Collection;
+import de.dnb.basics.applicationComponents.strings.StringUtils;
+import de.dnb.basics.collections.BiMap;
+import de.dnb.basics.collections.BiMultimap;
+import de.dnb.basics.collections.ListUtils;
+import utils.DB.GND_DB_UTIL;
+
+public class AH02 {
+
+	public final static int NAME = 2;
+	public final static int NID = NAME + 1;
+
+	public static void main(final String[] args)
+			throws ClassNotFoundException, IOException {
+		final BiMap<Integer, String> ppn2nid = GND_DB_UTIL.getppn2nid();
+		final BiMultimap<Integer, String> ppn2name = GND_DB_UTIL
+				.getppn2RDAName();
+		final String[][] table = StringUtils.readTableFromClip();
+
+		System.out.println(StringUtils.concatenateTab("Zeile Nr",
+				"Angeblicher Name", "richtiger Name", "NID"));
+
+		for (int i = 0; i < table.length; i++) {
+
+			final String nid = StringUtils.getCellAt(table, i, NID);
+			if (nid == null)
+				continue;
+			final Integer key = ppn2nid.getKey(nid);
+			if (key == null)
+				continue;
+			final int idn = key;
+
+			final Collection<String> names = ppn2name.get(idn);
+			if (names == null || names.isEmpty())
+				continue;
+			final String nameOfNID = ListUtils.getFirst(names);
+			final String name = StringUtils.getCellAt(table, i, NAME);
+
+			if (!StringUtils.equals(nameOfNID, name))
+				System.out.println(StringUtils.concatenateTab(i + 1, name,
+						nameOfNID, nid));
+
+		}
+
+	}
+
+}
