e26a7db3e59f798cf5826d63e8d304694fe8a811
Christian Baumann <k.c.baumann@t-online.de>
2025-06-02 19:52:22 +0200

    AH-02

--- src/baumann/musik/AH02.java
+++ src/baumann/musik/AH02.java
@@ -6,6 +6,7 @@ import de.dnb.basics.applicationComponents.strings.StringUtils;
 import de.dnb.basics.collections.BiMap;
 import de.dnb.basics.collections.BiMultimap;
 import de.dnb.basics.collections.ListUtils;
+import de.dnb.gnd.utils.IDNUtils;
 import utils.DB.GND_DB_UTIL;
 
 public class AH02 {
@@ -20,7 +21,7 @@ public class AH02 {
 				.getppn2RDAName();
 		final String[][] table = StringUtils.readTableFromClip();
 
-		System.out.println(StringUtils.concatenateTab("Zeile Nr",
+		System.out.println(StringUtils.concatenate(" / ", "Zeile Nr",
 				"Angeblicher Name", "richtiger Name", "NID"));
 
 		for (int i = 0; i < table.length; i++) {
@@ -28,19 +29,26 @@ public class AH02 {
 			final String nid = StringUtils.getCellAt(table, i, NID);
 			if (nid == null)
 				continue;
-			final Integer key = ppn2nid.getKey(nid);
-			if (key == null)
+			if (!IDNUtils.isKorrekteIDN(nid))
 				continue;
-			final int idn = key;
+			final Integer idn = ppn2nid.getKey(nid);
+			if (idn == null) {
+				System.out
+						.println(i + ": NID ohne zugehörige IDN in DB: " + nid);
+				continue;
+			}
 
 			final Collection<String> names = ppn2name.get(idn);
-			if (names == null || names.isEmpty())
+			if (names == null || names.isEmpty()) {
+				System.out.println(i + ": IDN ohne zugehörigen Namen in DB: "
+						+ idn + ", nid = " + nid);
 				continue;
+			}
 			final String nameOfNID = ListUtils.getFirst(names);
 			final String name = StringUtils.getCellAt(table, i, NAME);
 
 			if (!StringUtils.equals(nameOfNID, name))
-				System.out.println(StringUtils.concatenateTab(i + 1, name,
+				System.out.println(StringUtils.concatenate(" / ", i + 1, name,
 						nameOfNID, nid));
 
 		}
